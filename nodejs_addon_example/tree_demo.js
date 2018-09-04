659bb28fc609f5e81747e016721ccebf58e5ddf4
myc24601 <dichotomous@163.com>
2018-09-04 09:35:17 -0400

    tiny change

--- nodejs_addon_example/tree_demo.js
+++ nodejs_addon_example/tree_demo.js
@@ -12,11 +12,12 @@ addon.createDemoTree();
 
 http.createServer(function (req, res) {
     var fileName = req.url.slice(1, req.url.length);
-    fs.readFile(fileName, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    });
+    if (fileName) {
+        fs.readFile(fileName, function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+    } });
 }).listen(8000);
 
 // console.log(addon.hello());
